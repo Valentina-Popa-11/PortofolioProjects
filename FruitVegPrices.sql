5fcf430afff6da064a7f2357ea99e789f830cd7f
Valentina-Mariana Popa-Chipesu <103122142+Valentina-Popa-11@users.noreply.github.com>
2022-06-14 18:25:01 +0100

    Add files via upload

---
+++ FruitVegPrices.sql
@@ -0,0 +1,31 @@
+
+SELECT * 
+FROM PortofolioProject.dbo.FruitVegPrices
+
+-- Stardardize Date Format
+
+SELECT date, CONVERT(Date, date)
+FROM PortofolioProject.dbo.FruitVegPrices
+
+ALTER TABLE FruitVegPrices
+ADD DateConverted Date;
+
+UPDATE FruitVegPrices
+SET DateConverted = CONVERT(Date, date)
+
+-- Check categories included
+SELECT DISTINCT(category)
+FROM FruitVegPrices
+
+-- Select just fruits and vegetables
+SELECT category, item, variety, DateConverted AS date, price, unit
+FROM FruitVegPrices
+WHERE category = 'vegetable' OR category = 'fruit'
+ORDER BY DateConverted 
+
+-- Create a view for further visualization
+CREATE VIEW PriceOverTime AS 
+SELECT category, item, variety, DateConverted AS date, price, unit
+FROM FruitVegPrices
+WHERE category = 'vegetable' OR category = 'fruit'
+
