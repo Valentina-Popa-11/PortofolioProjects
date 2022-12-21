6e087e162eb1c31d988f4af23f4232227b304725
Valentina-Mariana Popa-Chipesu <103122142+Valentina-Popa-11@users.noreply.github.com>
2022-12-21 19:40:37 +0000

    Update FruitVegPrices.sql

--- FruitVegPrices.sql
+++ FruitVegPrices.sql
@@ -1,31 +1,63 @@
 
-SELECT * 
-FROM PortofolioProject.dbo.FruitVegPrices
+SELECT 
+      * 
+FROM 
+      PortofolioProject.dbo.FruitVegPrices
 
 -- Stardardize Date Format
 
-SELECT date, CONVERT(Date, date)
-FROM PortofolioProject.dbo.FruitVegPrices
+SELECT 
+      date, 
+      CONVERT(Date, date)
+FROM 
+      PortofolioProject.dbo.FruitVegPrices
 
-ALTER TABLE FruitVegPrices
+ALTER TABLE 
+      FruitVegPrices
 ADD DateConverted Date;
 
-UPDATE FruitVegPrices
-SET DateConverted = CONVERT(Date, date)
+UPDATE 
+      FruitVegPrices
+SET 
+      DateConverted = CONVERT(Date, date)
 
 -- Check categories included
-SELECT DISTINCT(category)
-FROM FruitVegPrices
+SELECT 
+      DISTINCT(category)
+FROM 
+      FruitVegPrices
 
 -- Select just fruits and vegetables
-SELECT category, item, variety, DateConverted AS date, price, unit
-FROM FruitVegPrices
-WHERE category = 'vegetable' OR category = 'fruit'
-ORDER BY DateConverted 
+SELECT 
+      category, 
+      item, 
+      variety, 
+      DateConverted AS date, 
+      price, 
+      unit
+FROM 
+      FruitVegPrices
+WHERE 
+      category = 'vegetable' 
+      OR 
+      category = 'fruit'
+ORDER BY 
+      DateConverted 
 
 -- Create a view for further visualization
-CREATE VIEW PriceOverTime AS 
-SELECT category, item, variety, DateConverted AS date, price, unit
-FROM FruitVegPrices
-WHERE category = 'vegetable' OR category = 'fruit'
+CREATE VIEW 
+      PriceOverTime AS 
+SELECT 
+      category, 
+      item, 
+      variety, 
+      DateConverted AS date, 
+      price, 
+      unit
+FROM 
+      FruitVegPrices
+WHERE 
+      category = 'vegetable' 
+      OR 
+      category = 'fruit'
 
